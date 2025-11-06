07d6bfa3f1c43fc9f7cde2486be2388ff56ec4e0
DatsOlena <datsolena@gmail.com>
2025-11-06 12:09:29 +0100

    Improve view

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -14,19 +14,19 @@ export default function Header({
   }
 
   return (
-    <header className="bg-color-primary text-inverse px-6 py-3 flex justify-between items-center shadow-theme-md relative" style={{ zIndex: 100 }}>
-      <h1 className="text-xl font-semibold">NTSuisse Demo</h1>
+    <header className="bg-color-primary sticky top-0 opacity-70 text-inverse px-6 py-3 flex justify-between items-center shadow-theme-md relative" style={{ zIndex: 100 }}>
+      <h1 className="text-xl font-semibold">Water analysis</h1>
       <div className="flex items-center gap-4">
         <nav className="flex gap-4">
           <button 
             onClick={() => handleNav('about')}
-            className={`hover:underline transition-opacity ${currentPage === 'about' ? 'underline' : 'opacity-90'}`}
+            className={`hover:underline transition-opacity uppercase ${currentPage === 'about' ? 'underline' : 'opacity-90'}`}
           >
             About
           </button>
           <button 
             onClick={() => handleNav('dashboard')}
-            className={`hover:underline transition-opacity ${currentPage === 'dashboard' ? 'underline' : 'opacity-90'}`}
+            className={`hover:underline transition-opacity uppercase ${currentPage === 'dashboard' ? 'underline' : 'opacity-90'}`}
           >
             Dashboard
           </button>
